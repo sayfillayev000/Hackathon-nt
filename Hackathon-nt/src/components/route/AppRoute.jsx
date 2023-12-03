a61f51684d7ef9efbdafa80899dbdbd24a853ee3
Muhammad <muhammadsayfillayev1027@gmail.com>
2023-12-03 22:36:02 +0500

    papka

--- Hackathon-nt/src/components/route/AppRoute.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AppRoute = ({ component: Component }) => {
-  return <Component />;
-};
-
-export default AppRoute;
--- Hackathon-nt/src/components/ui/ActionLink.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-const ActionLink = ({path, children}) => {
-  return <Link className='action-link' to={path}>{children}</Link>
-}
-
-export default ActionLink
