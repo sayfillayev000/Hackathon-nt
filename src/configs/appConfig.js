cd8a78878c6081a535682f57167b16e99689acc3
John Doe <hayotsultonov2005@mail.ru>
2023-12-03 17:03:32 +0500

    save files

--- src/configs/appConfig.js
+++
@@ -1,4 +0,0 @@
-export const appConfig = {
-    AuthentictedEntyPath : '/',
-    unAuthentictedEntyPath : '/register'
-}
--- src/pages/private/create/CreateAuthor.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreateAuthor = () => {
-  return (
-    <div>CreateAuthor</div>
-  )
-}
-
-export default CreateAuthor
